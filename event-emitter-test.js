22a9e18c3b9834c1d2c6d452fe71291543d534df
Sivakumar Balagopalan <Sivakumar.Balagopalan@outlook.com>
2024-07-28 17:09:02 -0700

    Fix the samples and add Inheritance sample

--- event-emitter-test.js
+++ event-emitter-test.js
@@ -1,94 +1,85 @@
-/* jshint esversion: 6 */
-
-describe('EventEmitter', function () {
-    'use strict';
-    describe('#addEventListener', function () {
-        it('shoudld add the listener for the given event', function () {
-            let ee = new EventEmitter();
-            ee.addListener('click', () => {});
-            ee.addListener('click', () => {});
-            ee.addListener('tap', () => {});
-            expect(ee.eventNames().length).to.be(2);
-            expect(ee.listeners('click').length).to.be(2);
-            expect(ee.listeners('tap').length).to.be(1);
-        });
+describe("EventEmitter", function () {
+  "use strict";
+  describe("#addEventListener", function () {
+    it("shoudld add the listener for the given event", function () {
+      let ee = new EventEmitter();
+      ee.addListener("click", () => { });
+      ee.addListener("click", () => { });
+      ee.addListener("tap", () => { });
+      expect(ee.eventNames().length).to.be(2);
+      expect(ee.listeners("click").length).to.be(2);
+      expect(ee.listeners("tap").length).to.be(1);
     });
-    describe('#removeEventListener', function () {
-        it('shoudld add the listener for the given event', function () {
+  });
+  describe("#removeEventListener", function () {
+    it("shoudld add the listener for the given event", function () {
+      let ee = new EventEmitter();
 
-            let ee = new EventEmitter();
+      let l1 = () => { };
+      let l2 = () => { };
+      let l3 = () => { };
 
-            let l1 = () => {};
-            let l2 = () => {};
-            let l3 = () => {};
+      ee.addListener("click", l1);
+      ee.addListener("click", l2);
+      ee.addListener("tap", l3);
 
-            ee.addListener('click', l1);
-            ee.addListener('click', l2);
-            ee.addListener('tap', l3);
+      ee.removeListener("click", l2);
+      ee.removeListener("tap", l3);
 
-            ee.removeListener('click', l2);
-            ee.removeListener('tap', l3);
-
-            expect(ee.eventNames().length).to.be(2);
-            expect(ee.listeners('click').length).to.be(1);
-            expect(ee.listeners('tap').length).to.be(0);
-        });
+      expect(ee.eventNames().length).to.be(2);
+      expect(ee.listeners("click").length).to.be(1);
+      expect(ee.listeners("tap").length).to.be(0);
     });
-    describe('#eventNames', function () {
-        it('shoudld add the listener for the given event', function () {
+  });
+  describe("#eventNames", function () {
+    it("shoudld add the listener for the given event", function () {
+      let ee = new EventEmitter();
 
-            let ee = new EventEmitter();
+      let l1 = () => { };
+      let l2 = () => { };
+      let l3 = () => { };
 
-            let l1 = () => {};
-            let l2 = () => {};
-            let l3 = () => {};
+      ee.addListener("click", l1);
+      ee.addListener("click", l2);
+      ee.addListener("tap", l3);
 
-            ee.addListener('click', l1);
-            ee.addListener('click', l2);
-            ee.addListener('tap', l3);
-
-            expect(ee.eventNames()).to.eql(['click', 'tap']);
-
-        });
+      expect(ee.eventNames()).to.eql(["click", "tap"]);
     });
-    describe('#eventNames', function () {
-        it('shoudld add the listener for the given event', function () {
+  });
+  describe("#eventNames", function () {
+    it("shoudld add the listener for the given event", function () {
+      let ee = new EventEmitter();
 
-            let ee = new EventEmitter();
+      let l1 = () => { };
+      let l2 = () => { };
+      let l3 = () => { };
 
-            let l1 = () => {};
-            let l2 = () => {};
-            let l3 = () => {};
+      ee.addListener("click", l1);
+      ee.addListener("tap", l3);
+      ee.addListener("click", l2);
 
-            ee.addListener('click', l1);
-            ee.addListener('tap', l3);
-            ee.addListener('click', l2);
-
-            expect(ee.listeners('click')).to.eql([l1, l2]);
-            expect(ee.listeners('tap')).to.eql([l3]);
-
-        });
+      expect(ee.listeners("click")).to.eql([l1, l2]);
+      expect(ee.listeners("tap")).to.eql([l3]);
     });
-    describe('#emit', function () {
-        it('shoudld add the listener for the given event', function () {
+  });
+  describe("#emit", function () {
+    it("shoudld add the listener for the given event", function () {
+      let ee = new EventEmitter();
+      let sum = 0;
+      let l1 = (a, b) => {
+        sum += a + b;
+      };
+      let l2 = (a, b) => {
+        sum += a + b;
+      };
+      let l3 = () => { };
 
-            let ee = new EventEmitter();
-            let sum = 0;
-            let l1 = (a, b) => {
-                sum += (a + b);
-            };
-            let l2 = (a, b) => {
-                sum += (a + b);
-            };
-            let l3 = () => {};
+      ee.addListener("click", l1);
+      ee.addListener("tap", l3);
+      ee.addListener("click", l2);
 
-            ee.addListener('click', l1);
-            ee.addListener('tap', l3);
-            ee.addListener('click', l2);
-
-            ee.emit('click', 1, 2)
-            expect(sum).to.be(6);
-
-        });
+      ee.emit("click", 1, 2);
+      expect(sum).to.be(6);
     });
+  });
 });
--- event-emitter.js
+++ event-emitter.js
@@ -1,37 +1,37 @@
-/* jshint esversion: 6*/
 class EventEmitter {
-    constructor() {
-        this._eventMap = new Map();
+  constructor() {
+    this._eventMap = new Map();
+  }
+  addListener(event, listener) {
+    if (!this._eventMap.has(event)) {
+      this._eventMap.set(event, []);
     }
-    addListener(event, listener) {
-        if (!this._eventMap.has(event)) {
-            this._eventMap.set(event, []);
-        }
-        this._eventMap.get(event).push(listener);
+    this._eventMap.get(event).push(listener);
+  }
+  removeListener(event, listener) {
+    if (!this._eventMap.has(event)) {
+      return;
+    } else {
+      let listeners = this._eventMap.get(event);
+      let index = listeners.indexOf(listener);
+      if (index >= 0) {
+        listeners.splice(index, 1);
+      }
     }
-    removeListener(event, listener) {
-        if (!this._eventMap.has(event)) {
-            return;
-        } else {
-            let listeners = this._eventMap.get(event);
-            let index = listeners.indexOf(listener);
-            if (index >= 0) {
-                listeners.splice(index, 1);
-            }
-        }
+  }
+  eventNames() {
+    // Create a shallow-copied array from iterable
+    return Array.from(this._eventMap.keys());
+  }
+  listeners(event) {
+    return Array.from(this._eventMap.get(event));
+  }
+  emit(event, ...args) {
+    if (!this._eventMap.has(event)) {
+      return;
+    } else {
+      let listeners = this._eventMap.get(event);
+      listeners.forEach((listener) => listener.call(null, ...args));
     }
-    eventNames() {
-        return Array.from(this._eventMap.keys()).slice(0);
-    }
-    listeners(event) {
-        return Array.from(this._eventMap.get(event)).slice(0);
-    }
-    emit(event, ...args) {
-        if (!this._eventMap.has(event)) {
-            return;
-        } else {
-            let listeners = this._eventMap.get(event);
-            listeners.forEach((listener) => listener.call(null, ...args));
-        }
-    }
+  }
 }
